747fae06c8420c47ffe0a2ce9203a8f5a3168383
Rosy <Rosy@Rosys-MacBook-Air.local>
2018-11-24 17:34:26 -0500

    agregué el modelo del contenido

--- contenido/models.py
+++ contenido/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Contenido(models.Model):
+    titulo=models.CharField(default="", max_length=25)
+    contenido1=models.CharField(default="", max_length=300)
+    titulo=models.ImageField(null=True)
