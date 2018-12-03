8c374e864d98e14c93c6f58d8adf84675eb3d6c7
hedleyQuintana <HedleyPty>
2018-12-03 17:35:39 -0500

    Usando JSONFields

--- contenido/models.py
+++ contenido/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from  django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+class Estudiante(User):
+    Aprobados=JSONField()
+    
+
+class Capitulo(models.Model):
+    nombre=models.CharField(default="", max_length=20)
+    
+    
 class Contenido(models.Model):
+    capitulo=models.ForeignKey(Capitulo, on_delete=models.CASCADE, default="")
     titulo=models.CharField(default="", max_length=25)
     contenido1=models.CharField(default="", max_length=300)
     imagen=models.ImageField(null=True)
