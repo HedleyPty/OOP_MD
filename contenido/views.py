248f053ee0269a6f435c5f0bfc1fdca1f30a1c58
hedleyQuintana <HedleyPty>
2018-12-14 06:26:00 -0500

    añadir parametros al URL

--- contenido/views.py
+++ contenido/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Capitulo 
 
 # Create your views here.
-def index(request):
-    return render(request, "index.html", context= {})
+def index(request,capitulo):
+    capt=Capitulo.objects.filter(id=capitulo)
+    nombre=capt[0].nombre
+    return render(request, "index.html", context= {"nombre":nombre})
 
